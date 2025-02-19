8e4d0003dc2906c8a883506220486edfb17be35a
Arthur G. Fakhouri <arthurfakhouri@outlook.com>
2025-02-19 20:00:38 -0300

    new: Getting all data from server and showing on each card of user

--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -1,11 +1,28 @@
+import {
+  getSubscribersSubscriberIdRankingClicks,
+  getSubscribersSubscriberIdRankingCount,
+  getSubscribersSubscriberIdRankingPosition,
+} from '@/http/api'
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
+import { twMerge } from 'tailwind-merge'
 
-export function Stats() {
+type StatsProps = {
+  subscriberId: string
+}
+
+export async function Stats({ subscriberId }: StatsProps) {
+  const { count: accessCount } =
+    await getSubscribersSubscriberIdRankingClicks(subscriberId)
+  const { count: inviteCount } =
+    await getSubscribersSubscriberIdRankingCount(subscriberId)
+  const { position: rankingPosition } =
+    await getSubscribersSubscriberIdRankingPosition(subscriberId)
+
   return (
     <div className="grid gap-3 md:grid-cols-3">
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          1042
+          {accessCount}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Acessos ao link
@@ -15,7 +32,7 @@ export function Stats() {
       </div>
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          875
+          {inviteCount}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Inscrições feitas
@@ -24,14 +41,25 @@ export function Stats() {
         <BadgeCheck className="absolute top-3 left-3 size-5 text-blue" />
       </div>
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
-        <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          3°
+        <span
+          className={
+            'font-heading text-2xl font-semibold leading-none text-gray-200'
+          }
+        >
+          {rankingPosition ? `${rankingPosition}°` : '-'}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Posição no ranking
         </span>
 
-        <Medal className="absolute top-3 left-3 size-5 text-bronze" />
+        <Medal
+          className={twMerge(
+            'absolute top-3 left-3 size-5 text-purple',
+            rankingPosition === 1 ? 'text-gold' : '',
+            rankingPosition === 2 ? 'text-silver' : '',
+            rankingPosition === 3 ? 'text-bronze' : ''
+          )}
+        />
       </div>
     </div>
   )
