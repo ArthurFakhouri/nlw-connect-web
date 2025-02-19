21c760d57a8088964d1e3ec411ff027462dd8cb1
Arthur G. Fakhouri <arthurfakhouri@outlook.com>
2025-02-19 19:57:48 -0300

    new: Adding disabled style to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,11 @@
-import { ComponentProps } from "react"
+import type { ComponentProps } from 'react'
 
 type ButtonProps = ComponentProps<'button'>
 
-export function Button({
-  ...props
-}: ButtonProps) {
+export function Button({ ...props }: ButtonProps) {
   return (
     <button
-      className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className="flex justify-between items-center px-5 h-12 bg-gray-500 text-blue font-semibold rounded-xl w-full cursor-pointer transition-colors duration-300 hover:not-disabled:bg-blue hover:not-disabled:text-gray-900 disabled:opacity-70 hover:disabled:cursor-not-allowed"
       {...props}
     />
   )
